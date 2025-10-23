0214f3a54770210a2662974fb017b8f83391a8c9
miniatourkiller <jgathiru91@gmail.com>
2025-10-24 02:02:04 +0300

    working properly, will add all

--- src/main/java/com/gym/GoldenGym/entities/Order.java
+++ src/main/java/com/gym/GoldenGym/entities/Order.java
@@ -8,12 +8,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "tb_orders")
 public class Order extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/gym/GoldenGym/entities/User.java
+++ src/main/java/com/gym/GoldenGym/entities/User.java
@@ -6,12 +6,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "tb_users")
 public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
